ba9cde9b10431820c426bf8947edd8fd014e687d
Zackery Leman <zleman1593@gmail.com>
2020-03-29 21:35:15 -0700

    async

--- packages/dd-trace/src/exporters/honeycomb/index.js
+++ packages/dd-trace/src/exporters/honeycomb/index.js
@@ -4,25 +4,23 @@
 // const Writer = require("./writer");
 // const Scheduler = require("./scheduler");
 const libHoney = require("libhoney");
-const  createHash  = require('crypto').createHash;
+const createHash = require("crypto").createHash;
 
 class HoneycombExporter {
   constructor({ url, hostname, port, flushInterval }, prioritySampler) {
-  //   this._url = url || new URL(`http://${hostname || "localhost"}:${port}`);
-  //   this._writer = new Writer(this._url, prioritySampler);
-
-  //   if (flushInterval > 0) {
-  //     this._scheduler = new Scheduler(
-  //       () => this._writer.flush(),
-  //       flushInterval
-  //     );
-  //   }
-  //   this._scheduler && this._scheduler.start();
+    //   this._url = url || new URL(`http://${hostname || "localhost"}:${port}`);
+    //   this._writer = new Writer(this._url, prioritySampler);
+    //   if (flushInterval > 0) {
+    //     this._scheduler = new Scheduler(
+    //       () => this._writer.flush(),
+    //       flushInterval
+    //     );
+    //   }
+    //   this._scheduler && this._scheduler.start();
   }
 
-  export(spans,rootSpan) {
-
-    return HoneycombDataDogForwarder.forwardTrace(spans,rootSpan);
+  export(spans, rootSpan) {
+    return HoneycombDataDogForwarder.forwardTrace(spans, rootSpan);
     // this._writer.append(spans);
 
     // if (!this._scheduler) {
@@ -34,25 +32,24 @@ class HoneycombExporter {
 module.exports = HoneycombExporter;
 
 let _honeycombSdk;
-  /**
-   * Initialize Honeycomb library
-   */
-  function getHoneycombSdk() {
-    if (_honeycombSdk) {
-      return _honeycombSdk;
-    }
-    if (process.env.HONEYCOMB_WRITE_KEY) {
-      _honeycombSdk = new libHoney({
-        writeKey: process.env.HONEYCOMB_WRITE_KEY,
-        // This is WELL Specific atm
-        dataset: `well-${process.env.SERVICE}-${process.env.ENV.toLowerCase()}`,
-      });
-    }
+/**
+ * Initialize Honeycomb library
+ */
+function getHoneycombSdk() {
+  if (_honeycombSdk) {
     return _honeycombSdk;
   }
+  if (process.env.HONEYCOMB_WRITE_KEY) {
+    _honeycombSdk = new libHoney({
+      writeKey: process.env.HONEYCOMB_WRITE_KEY,
+      // This is WELL Specific atm
+      dataset: `well-${process.env.SERVICE}-${process.env.ENV.toLowerCase()}`
+    });
+  }
+  return _honeycombSdk;
+}
 // Singleton https://refactoring.guru/design-patterns/singleton/typescript/example
- class HoneycombDataDogForwarder {
-
+class HoneycombDataDogForwarder {
   /**
    * Have to call this immediately before span.finish()
    * Will extract sample rates, create formatted spans for the trace and send the sampled trace to honeycomb
@@ -60,31 +57,34 @@ let _honeycombSdk;
    * @param ddSpan
    * @returns Void
    */
-  static forwardTrace(spans,rootSpan) {
+  static forwardTrace(spans, rootSpan) {
     try {
       // If there is parent ID don't try to send to honeycomb yet. Only do it for the root span.
       if (rootSpan._parentId == null) {
         // If there is parent ID don't try to send to honeycomb yet. Only do it for the root span.
         const rootSampleRate =
-        rootSpan.getBaggageItem('rootSampleRate') != null ? parseInt(rootSpan.getBaggageItem('rootSampleRate'), 10) : undefined;
+          rootSpan.getBaggageItem("rootSampleRate") != null
+            ? parseInt(rootSpan.getBaggageItem("rootSampleRate"), 10)
+            : undefined;
         const childSpanSampleRate =
-        rootSpan.getBaggageItem('childSpanSampleRate') != null ? parseInt(rootSpan.getBaggageItem('childSpanSampleRate'), 10) : undefined;
+          rootSpan.getBaggageItem("childSpanSampleRate") != null
+            ? parseInt(rootSpan.getBaggageItem("childSpanSampleRate"), 10)
+            : undefined;
         const slowTraceLimitMs =
-        rootSpan.getBaggageItem('slowTraceLimitMs') != null ? parseInt(rootSpan.getBaggageItem('slowTraceLimitMs'), 10) : undefined;
+          rootSpan.getBaggageItem("slowTraceLimitMs") != null
+            ? parseInt(rootSpan.getBaggageItem("slowTraceLimitMs"), 10)
+            : undefined;
         return HoneycombDataDogForwarder.sendTraceToHoneycomb({
           spans,
           rootSampleRate: rootSampleRate,
           childSpanSampleRate: childSpanSampleRate || rootSampleRate,
-          slowTraceLimitMs,
+          slowTraceLimitMs
         });
-
       }
     } catch (error) {
       console.error(error);
     }
   }
-
-
 
   /**
    * Takes a trace (array of spans) generated from Datadog and sends it to honeycomb with sampling
@@ -95,11 +95,11 @@ let _honeycombSdk;
    * @param slowTraceLimitMs Number in ms. How long in milliseconds a trace needs to take before being considered slow
    * @returns Void
    */
-  static sendTraceToHoneycomb({
+  static async sendTraceToHoneycomb({
     spans,
     rootSampleRate = 1,
     childSpanSampleRate = 1,
-    slowTraceLimitMs = 3000,
+    slowTraceLimitMs = 3000
   }) {
     const hny = getHoneycombSdk();
     if (hny == null) {
@@ -112,19 +112,24 @@ let _honeycombSdk;
     //TODO: const isError = spans[0].isError;
     if (isError) {
       // Sample all errors
-      console.info({ traceId }, 'trace with errors');
+      console.info({ traceId }, "trace with errors");
       childSpanSampleRate = 1;
       rootSampleRate = 1;
     }
     const isSlow = rootSpan.duration_ms > slowTraceLimitMs;
     if (isSlow) {
-      console.info({ traceId, duration_ms: rootSpan.duration_ms }, 'slow trace');
+      console.info(
+        { traceId, duration_ms: rootSpan.duration_ms },
+        "slow trace"
+      );
       // Sample all very slow traces since they are more likely to be interesting
       childSpanSampleRate = 1;
       rootSampleRate = 1;
     }
 
-    const { shouldSample: shouldSampleTrace } = HoneycombDataDogForwarder.deterministicSampler(rootSampleRate, traceId);
+    const {
+      shouldSample: shouldSampleTrace
+    } = HoneycombDataDogForwarder.deterministicSampler(rootSampleRate, traceId);
     if (shouldSampleTrace) {
       const event = hny.newEvent();
       event.timestamp = new Date(rootSpan.timestamp);
@@ -132,11 +137,16 @@ let _honeycombSdk;
       event.add(rootSpan);
       event.add({ childSpanCount: spans.length });
       let spanPromises = [event.sendPresampled()];
-      const { shouldSample: shouldSampleChildSpans } = HoneycombDataDogForwarder.deterministicSampler(childSpanSampleRate, traceId);
+      const {
+        shouldSample: shouldSampleChildSpans
+      } = HoneycombDataDogForwarder.deterministicSampler(
+        childSpanSampleRate,
+        traceId
+      );
       // Will only sample child spans if trace will be kept
 
       if (shouldSampleChildSpans) {
-        spans.forEach((span) => {
+        spans.forEach(span => {
           //Todo: Add server stats here
           const event = hny.newEvent();
           event.add(span);
@@ -158,7 +168,7 @@ let _honeycombSdk;
   static deterministicSampler(sampleRate, traceId) {
     const MAX_UINT32 = Math.pow(2, 32) - 1;
 
-    const sum = createHash('sha1')
+    const sum = createHash("sha1")
       .update(traceId)
       .digest();
 
@@ -166,9 +176,7 @@ let _honeycombSdk;
 
     return {
       shouldSample: sum.readUInt32BE(0) <= upperBound,
-      sampleRate,
+      sampleRate
     };
   }
-
 }
-
