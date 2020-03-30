87a6b71a3178cdd98eed685b5052bbd5a472f0c7
Zackery Leman <zleman1593@gmail.com>
2020-03-29 17:14:00 -0700

    Add hoenycomb

---
+++ packages/dd-trace/src/exporters/honeycomb/index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const URL = require("url-parse");
+const Writer = require("./writer");
+const Scheduler = require("./scheduler");
+const libHoney = require("libhoney");
+
+class HoneycombExporter {
+  constructor({ url, hostname, port, flushInterval }, prioritySampler) {
+    this._url = url || new URL(`http://${hostname || "localhost"}:${port}`);
+    this._writer = new Writer(this._url, prioritySampler);
+
+    if (flushInterval > 0) {
+      this._scheduler = new Scheduler(
+        () => this._writer.flush(),
+        flushInterval
+      );
+    }
+    this._scheduler && this._scheduler.start();
+  }
+
+  export(spans) {
+    this._writer.append(spans);
+
+    if (!this._scheduler) {
+      this._writer.flush();
+    }
+  }
+}
+
+module.exports = HoneycombExporter;
